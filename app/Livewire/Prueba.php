e176d4660a6195790293e41d1ed33942980cea98
Emmanuel Espinoza <eespinest87@gmail.com>
2023-11-30 18:15:30 -0600

    Commit inicial

---
+++ app/Livewire/Prueba.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Prueba extends Component
+{
+
+    public $title = "Botón";
+
+    public function render()
+    {
+        return view('livewire.prueba');
+    }
+}
