d8fe65f868fbe71c4b03c00ba291487c3d5df9f6
Emmanuel Espinoza <eespinest87@gmail.com>
2023-12-27 14:23:29 -0600

    Corrección de vista - 271223

--- resources/views/web/catalogos/cat_empleados.blade.php
+++ resources/views/web/catalogos/cat_empleados.blade.php
@@ -1,77 +1,37 @@
 @extends('adminlte::page')
 
-@section('title', 'AEXA')
-
-@section('content_header')
-<h2>Catálogo de empleados</h2>
-@stop
-
 @section('content')
-@include('modals.modal_nuevo_empleado')
-<div class="container">
-    <div class="row">
-        <div class="col-10 text-left">
-            
-        </div class="col-2">
-        <x-adminlte-button label="Nuevo empleado" data-toggle="modal" data-target="#modalEmpleado" theme="primary"
-            icon="fas fa-plus" />
-    </div>
-    <br>
-    <div class="row">
-        <div class="col-md-12">
-            {{-- Setup data for datatables --}}
-            @php
-            $heads = [
-            'ID',
-            'Name',
-            ['label' => 'Phone', 'width' => 40],
-            ['label' => 'Actions', 'no-export' => true, 'width' => 5],
-            ];
-
-            $btnEdit = '<button class="btn btn-xs btn-default text-primary mx-1 shadow" title="Edit">
-                <i class="fa fa-lg fa-fw fa-pen"></i>
-            </button>';
-            $btnDelete = '<button class="btn btn-xs btn-default text-danger mx-1 shadow" title="Delete">
-                <i class="fa fa-lg fa-fw fa-trash"></i>
-            </button>';
-            $btnDetails = '<button class="btn btn-xs btn-default text-teal mx-1 shadow" title="Details">
-                <i class="fa fa-lg fa-fw fa-eye"></i>
-            </button>';
-
-            $config = [
-            'data' => [
-            [22, 'John Bender', '+02 (123) 123456789', '<nobr>'.$btnEdit.$btnDelete.$btnDetails.'</nobr>'],
-            [19, 'Sophia Clemens', '+99 (987) 987654321', '<nobr>'.$btnEdit.$btnDelete.$btnDetails.'</nobr>'],
-            [3, 'Peter Sousa', '+69 (555) 12367345243', '<nobr>'.$btnEdit.$btnDelete.$btnDetails.'</nobr>'],
-            ],
-            'order' => [[1, 'asc']],
-            'columns' => [null, null, null, ['orderable' => false]],
-            ];
-            @endphp
-
-            {{-- Minimal example / fill data using the component slot --}}
-            <x-adminlte-datatable id="table1" :heads="$heads">
-                @foreach($config['data'] as $row)
-                <tr>
-                    @foreach($row as $cell)
-                    <td>{!! $cell !!}</td>
-                    @endforeach
-                </tr>
-                @endforeach
-            </x-adminlte-datatable>
+    <section class="section">
+        <div class="section-header">
+            <div class="row">
+                <h3 class="page__header col-4">Empleados</h3>
+                <div class="col-4"></div>
+                <div class="col-4">
+                    <p class="text-primary text-right">Terminal:</p>
+                </div>
+            </div>
         </div>
-    </div>
-</div>
-
-@stop
-
-
-@section('css')
-<link rel="stylesheet" href="/css/admin_custom.css">
-@stop
-
-@section('js')
-<script>
-    console.log('Hi!'); 
-</script>
-@stop
+        <div class="section-body">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <table id="empleados" class="table table-responsive-sm table-striped table-bordered mt-2">
+                                <thead>
+                                    <th></th>
+                                    <th></th>
+                                    <th></th>
+                                    <th></th>
+                                    <th></th>
+                                    <th></th>
+                                    <th></th>
+                                    <th></th>
+                                </thead>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
