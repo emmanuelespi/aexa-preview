95e656363eb25c2fa47d54dcae4d15a68ebc4105
Emmanuel Espinoza <eespinest87@gmail.com>
2023-12-27 13:47:28 -0600

    Se agregó ruta de vista taquillas

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TerminalController;
 use App\Http\Controllers\EmpresaController;
+use App\Http\Controllers\TaquillaController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -43,8 +44,10 @@ Route::middleware('auth')->group(function () {
         Route::get('/clientes', fn() => view('web.catalogos.cat_clientes'))->name('cat_clientes');
         Route::get('/empresas', [EmpresaController::class, 'index'])->name('cat_empresas');
         Route::get('/terminales', [TerminalController::class, 'index'])->name('cat_terminales');
+        Route::get('/taquillas', [TaquillaController::class, 'index'])->name('cat_taquillas');
         Route::get('/precios', fn() => view('web.catalogos.cat_precios'))->name('cat_precios');
-        Route::get('/corridas', fn() => view('web.catalogos.cat_corridas'))->name('cat_corridas');        
+        Route::get('/corridas', fn() => view('web.catalogos.cat_corridas'))->name('cat_corridas');
+        
     });
 
     Route::prefix('/monitoreo')->group(function(){
