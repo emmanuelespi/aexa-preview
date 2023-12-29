2283ff3dfeb721035e68fc5a1fc106aad18fdb58
Emmanuel Espinoza <eespinest87@gmail.com>
2023-12-28 18:13:47 -0600

    Corrección de vista y creación de controlador - 281223

--- resources/views/web/roles/index.blade.php
+++ resources/views/web/roles/index.blade.php
@@ -1,10 +1,45 @@
 @extends('adminlte::page')
 
-@section('title', 'AEXA | Roles')
-
 @section('content')
-
+<section class="section">
+    <div class="section-header">
+        <div class="row">
+            <h3 class="page__heading col-4">Empresas</h3>
+            <div class="col-4"></div>
+            <div class="col-4">
+                <p class="text-primary text-right">Empresa: </p>
+            </div>
+        </div>
+    </div>
+    <div class="section-body">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-body">
+                        <table id="roles" class="table table-responsive-sm table-striped table-bordered mt-2">
+                            <thead>
+                                <th>Nombre</th>
+                                <th>Acciones</th>
+                            </thead>
+                            <tbody>
+                                @foreach ($roles as $role)
+                                    <tr>
+                                        <td>{{ $role->name }}</td>
+                                        <td>1</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 @endsection
 
 @section('js')
+    <script>
+        console.log('Sirve el JS')
+    </script>
 @endsection
