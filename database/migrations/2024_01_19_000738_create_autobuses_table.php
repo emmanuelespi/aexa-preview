1fdb04f987ea9dd429f52a1b64cbb746df955f6c
Emmanuel Espinoza <eespinest87@gmail.com>
2024-01-19 18:22:10 -0600

    Creación de migración y corrección de vista autobuses - 190124

--- database/migrations/2024_01_19_000738_create_autobuses_table.php
+++ database/migrations/2024_01_19_000738_create_autobuses_table.php
@@ -13,7 +13,26 @@ return new class extends Migration
     {
         Schema::create('autobuses', function (Blueprint $table) {
             $table->id();
+            $table->string('tipo');
+            $table->string('modelo');
+            $table->string('catalogo');
+            $table->string('numero_serie');
+            $table->string('numero_motor');
+            $table->string('marca');
+            $table->string('placa');
+            $table->integer('capacidad');
+            $table->integer('numero_economico');
+            $table->integer('servicio');
+            $table->dateTime('fecha_compra');
+            $table->dateTime('fecha_verificacion');
+            $table->dateTime('fecha_inicio_seguro');
+            $table->dateTime('fecha_fin_seguro');
+            $table->tinyInteger('estatus')->default(1);
             $table->timestamps();
+            $table->softDeletes();
+            $table->unsignedBigInteger('id_empresa');
+            $table->foreign('id_empresa')->references('id')->on('empresa');
+            $table->engine = 'InnoDB';
         });
     }
 
--- resources/views/web/catalogos/cat_autobuses.blade.php
+++ resources/views/web/catalogos/cat_autobuses.blade.php
@@ -1,20 +1,39 @@
 @extends('adminlte::page')
 
-@section('title', 'AEXA')
-
-@section('content_header')
-    <h1>Aexa | Preview</h1>
-@stop
-
 @section('content')
-    <h2>Autobuses</h2>
-@stop
-
-
-@section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
-@stop
-
-@section('js')
-    <script> console.log('Hi!'); </script>
-@stop
+    <section class="section">
+        <div class="section-header">
+            <div class="row">
+                <h3 class="page__heading col-4">Autobuses</h3>
+                <div class="col-4"></div>
+                <div class="col-4">
+                    <p class="text-primary text-right">Empresa: </p>
+            </div>
+            </div>
+        </div>
+        <div class="section-body">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <table id="empresas" class="table table-responsive-sm table-striped table-bordered mt-2">
+                                <thead>
+                                    <th>Nombre</th>
+                                    <th>Clave</th>
+                                    <th>Nombre</th>
+                                    <th>Razón social</th>
+                                    <th>RFC</th>
+                                    <th>Estatus</th>
+                                    <th>Acciones</th>
+                                </thead>
+                                <tbody>
+                                    
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
