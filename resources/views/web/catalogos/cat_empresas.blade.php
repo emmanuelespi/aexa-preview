bada0522321e997e7de1a919ce5b073b74ac72cb
Emmanuel Espinoza <eespinest87@gmail.com>
2024-01-29 17:50:17 -0600

    Integracion de datatable en empresa - 290124

--- resources/views/web/catalogos/cat_empresas.blade.php
+++ resources/views/web/catalogos/cat_empresas.blade.php
@@ -61,7 +61,35 @@
 @endsection
 
 @section('js')
-    <script>
-        console.log('Sirve el JS')
-    </script>
+<script>
+    $(function() {
+        $("#empresas").DataTable({
+            "paging": true,
+            "lengthChange": true,
+            "searching": true,
+            "ordering": true,
+            "info": true,
+            "autoWidth": false,
+            "lengthMenu": [5, 10, 25, 50],
+            language: {
+                "processing": "Procesando...",
+                "lengthMenu": "Mostrar _MENU_ registros",
+                "zeroRecords": "No se encontraron resultados",
+                "emptyTable": "Ningún dato disponible en esta tabla",
+                "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+                "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+                "search": "Buscar:",
+                "infoThousands": ",",
+                "loadingRecords": "Cargando...",
+                "paginate": {
+                "first": "Primero",
+                "last": "Último",
+                "next": "Siguiente",
+                "previous": "Anterior"
+                },
+                "info": "Mostrando _START_ a _END_ de _TOTAL_ registros",
+            }
+        });
+    });
+</script>
 @endsection
