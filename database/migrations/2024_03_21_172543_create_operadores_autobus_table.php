670707d4d10d2e5117f935b9689ebc3c8e066628
Emmanuel Espinoza <eespinest87@gmail.com>
2024-03-21 11:33:40 -0600

    Creación de migracion tabla intermedia operadores autobus - 210324

---
+++ database/migrations/2024_03_21_172543_create_operadores_autobus_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('operadores_autobus', function (Blueprint $table) {
+            $table->id();
+            $table->tinyInteger('estatus')->default(1);
+            $table->timestamps();
+            $table->softDeletes();
+            $table->unsignedBigInteger('operador_id');
+            $table->foreign('operador_id')->references('id')->on('operadores');
+            $table->unsignedBigInteger('autobus_id');
+            $table->foreign('autobus_id')->references('id')->on('autobus');
+            $table->engine = 'InnoDB';
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('operadores_autobus');
+    }
+};
