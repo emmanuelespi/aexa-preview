5df1e1bc28b2c9635c769e6d10ac338e3d6b67bc
Emmanuel Espinoza <eespinest87@gmail.com>
2024-03-21 13:19:17 -0600

    Creacion migracion dias venta - 210324

---
+++ database/migrations/2024_03_21_191127_create_dias_venta_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dias_venta', function (Blueprint $table) {
+            $table->id();
+            $table->string('dia_semana',25);
+            $table->tinyInteger('estatus')->default();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->engine = 'InnoDB';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dias_venta');
+    }
+};
