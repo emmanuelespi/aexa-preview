c729a3ebdd16c6f68443c4286c4f1b5b7b618d71
Emmanuel Espinoza <eespinest87@gmail.com>
2024-03-21 12:50:31 -0600

    Creacion migracion escalas - 210324

---
+++ database/migrations/2024_03_21_183921_create_escalas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('escalas', function (Blueprint $table) {
+            $table->id();
+            $table->string('terminal_origen');
+            $table->string('terminal_destino');
+            $table->tinyInteger('estatus')->default(1);
+            $table->integer('minutos_distancia');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->unsignedBigInteger('plantilla_viaje_id');
+            $table->foreign('plantilla_viaje_id')->references('id')->on('plantilla_viaje');
+            $table->engine = 'InnoDB';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('escalas');
+    }
+};
