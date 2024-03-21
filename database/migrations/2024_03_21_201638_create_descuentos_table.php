3517973e61a743e76405d7e01979b0823a698f87
Emmanuel Espinoza <eespinest87@gmail.com>
2024-03-21 14:19:35 -0600

    Creacion migracion descuentos - 210324

---
+++ database/migrations/2024_03_21_201638_create_descuentos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('descuentos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre_descuento', 20);
+            $table->double('porcentaje');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->engine = 'InnoDB';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('descuentos');
+    }
+};
