e44f4d8d69c1e4fa5c3f27e09fe3f155f17240ec
Emmanuel Espinoza <eespinest87@gmail.com>
2024-03-21 17:53:25 -0600

    Creacion migracion detalle ventas - 210324

---
+++ database/migrations/2024_03_21_234627_create_detalle_ventas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('detalle_ventas', function (Blueprint $table) {
+            $table->id();
+            $table->string('producto', 50);
+            $table->string('tipo_producto', 80);
+            $table->integer('cantidad');
+            $table->double('subtotal');
+            $table->double('total');
+            $table->integer('aexa_nip_puntos_generados');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->unsignedBigInteger('ventas_id');
+            $table->foreign('ventas_id')->references('id')->on('ventas');
+            $table->engine = 'InnoDB';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('detalle_ventas');
+    }
+};
